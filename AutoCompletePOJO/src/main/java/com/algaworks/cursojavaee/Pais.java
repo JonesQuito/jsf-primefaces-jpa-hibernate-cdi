0f522b145d0dcb24b62414348871da1d2ef3503e
JonesQuito <jonesdhy@hotmail.com>
2018-02-10 17:02:55 -0200

    AutoComplete com suporte a POJO

---
+++ AutoCompletePOJO/src/main/java/com/algaworks/cursojavaee/Pais.java
@@ -0,0 +1,12 @@
+package com.algaworks.cursojavaee;
+
+import java.io.Serializable;
+
+public class Pais implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer codigo;
+	private String nome;
+
+}
---
+++ AutoCompletePOJO/src/main/java/com/algaworks/cursojavaee/PaisConverter.java
@@ -0,0 +1,41 @@
+package com.algaworks.cursojavaee;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter("paisConverter")
+public class PaisConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		Integer codigo = null;
+		if(value != null) {
+			System.out.println("getAsObject: " + value);
+			
+			try {
+				codigo = new Integer(value);
+			}catch (NumberFormatException e) {
+				context.addMessage(null, new FacesMessage("Erro ao Converter o país"));
+			}
+			
+			for(Pais pais : PerfilUsuarioBean.PAISES) {
+				if(codigo.equals(pais.getCodigo())) {
+					return pais;
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		if(value != null && !value.equals("")) {
+			return ((Pais)value).getCodigo().toString();
+		}
+		return null;
+	}
+
+}
